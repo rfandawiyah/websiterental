deb89d4ee72142f5bc45d4c525fe115b3e55731f
Endyfadlullah <endinewjr68@gmail.com>
2024-05-24 10:27:28 +0700

    control

--- app/Http/Controllers/loginController.php
+++ app/Http/Controllers/loginController.php
@@ -7,36 +7,35 @@ use Illuminate\Support\Facades\Auth;
 
 class loginController extends Controller
 {
-    public function index() {
+    public function index()
+    {
         return view('auth.login');
     }
 
-    public function login(Request $request) 
+    public function login(Request $request)
     {
         $credentials = $request->validate([
             'name' => ['required'],
             'password' => ['required'],
         ]);
- 
+
         if (Auth::attempt($credentials)) {
             $request->session()->regenerate();
- 
+
             return redirect()->intended('dashboard');
         }
- 
-        // return back()->withErrors([
-        //     'name' => 'The provided credentials do not match our records.',
-        // ])->onlyInput('name');
+
+        return back()->with('loginError', 'Login Failed');
     }
 
     public function logout(Request $request)
-{
-    Auth::logout();
- 
-    $request->session()->invalidate();
- 
-    $request->session()->regenerateToken();
- 
-    return redirect('/');
-}
+    {
+        Auth::logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('/');
+    }
 }
--- app/Http/Controllers/pelangganController.php
+++ app/Http/Controllers/pelangganController.php
@@ -3,11 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class pelangganController extends Controller
 {
     public function index()
     {
+        if (!Auth::check()) {
+            return redirect('/');
+        }
+
         return view('pelanggan.pelanggan');
     }
 }
